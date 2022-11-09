0e52e374fcb21a1e24e8f005cfd6f2c837fac7f6
mohammadnazamikit <108289537+mohammadnazamikit@users.noreply.github.com>
2022-11-09 16:09:40 +0100

    update

---
+++ src/blog/jwtAuth.js
@@ -0,0 +1,49 @@
+import createHttpError from "http-errors";
+import jwt from "jsonwebtoken";
+
+export const JWTAuthMiddleware = async (req, res, next) => {
+  if (!req.headers.authorization) {
+    next(
+      createHttpError(
+        401,
+        "please provide bearer token in the authorization header"
+      )
+    );
+  } else {
+    try {
+      const accessToken = req.headers.authorization.replace("Bearer ", "");
+
+      const payload = await verifyAccessToken(accessToken);
+
+      req.user = {
+        _id: payload._id,
+        role: payload.role,
+      };
+    } catch (error) {
+      next(createHttpError(401, "Token not Valid"));
+    }
+  }
+};
+
+export const createAccessToken = (payload) => {
+  new Promise((resolve, reject) => {
+    jwt.sign(
+      payload,
+      process.env.JWT_SECRET,
+      { expiresIn: "1 week" },
+      (err, token) => {
+        if (err) reject(err);
+        else resolve(token);
+      }
+    );
+  });
+};
+
+export const verifyAccessToken = (accessToken) => {
+  new Promise((res, rej) => {
+    jwt.verify(accessToken, process.env.JWT_SECRET, (err, originalPayload) => {
+      if (err) rej(err);
+      else res(originalPayload);
+    });
+  });
+};
---
+++ src/user/model.js
@@ -0,0 +1,43 @@
+import mongoose from "mongoose";
+import bcrypt from "bcrypt";
+
+const { Schema, model } = mongoose;
+
+const usersSchema = new Schema(
+  {
+    firstName: { type: String, required: true },
+    lastName: { type: String, required: true },
+    email: { type: String, required: true },
+    password: { type: String, required: true },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+usersSchema.static("checkCredentials", async function (email, plainPassword) {
+  const user = await this.findOne({ email });
+  if (user) {
+    const isMatch = await bcrypt.compare(plainPassword, user.password);
+    if (isMatch) {
+      return user;
+    } else {
+      return null;
+    }
+  } else {
+    return null;
+  }
+});
+
+usersSchema.pre("save", async function (next) {
+  const currentUser = this;
+  if (currentUser.isModified("password")) {
+    const plainPW = currentUser.password;
+
+    const hash = await bcrypt.hash(plainPW, 10);
+    currentUser.password = hash;
+  }
+  next();
+});
+
+export default model("user", usersSchema);
